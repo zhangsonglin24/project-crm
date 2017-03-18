fbfb64031c77bf12eb5c65bcb453abcb18fb65e7
zhangsonglin24 <13183136301@163.com>
2017-03-18 10:45:18 +0800

    ssh配置

---
+++ src/main/java/com/crm/pojo/User.java
@@ -0,0 +1,18 @@
+package com.crm.pojo;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class User {
+
+    private Integer id;
+    private String username;
+    private String password;
+    private String realname;
+    private String weixin;
+    private Timestamp createtime;
+    private Integer roleid;
+
+}
