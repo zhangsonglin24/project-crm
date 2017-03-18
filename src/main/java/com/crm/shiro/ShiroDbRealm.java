1c52ae9427961049975fffcdfc556fca825a298b
sunxiaoyun21 <479338314@qq.com>
2017-03-18 11:13:26 +0800

    权限框架的配置

---
+++ src/main/java/com/crm/shiro/ShiroDbRealm.java
@@ -0,0 +1,35 @@
+package com.crm.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by Administrator on 2017/3/18.
+ */
+public class ShiroDbRealm extends AuthorizingRealm {
+    /**
+     * 权限认证
+     * @param principalCollection
+     * @return
+     */
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    /**
+     * 登录认证
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
---
+++ src/main/java/com/crm/shiro/ShiroUtil.java
@@ -0,0 +1,26 @@
+package com.crm.shiro;
+
+import com.crm.pojo.User;
+import org.apache.shiro.SecurityUtils;
+
+/**
+ * Created by Administrator on 2017/3/18.
+ */
+public class ShiroUtil {
+
+    public static User  getCurrentUser(){
+        return (User) SecurityUtils.getSubject().getPrincipal();
+    }
+
+    public static String getCurrentRealName(){
+       return getCurrentUser().getRealname();
+    }
+
+    public static Integer getCurrentUserId(){
+        return getCurrentUser().getId();
+    }
+
+    public static String getCurrentUserName(){
+        return getCurrentUser().getUsername();
+    }
+}
